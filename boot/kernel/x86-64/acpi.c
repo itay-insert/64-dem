6eade61838f859c631117c81b8430c32526b84f8
itay-insert <itaymademjunyor@gmail.com>
2026-07-12 17:54:13 +0300

    returning

--- boot/kernel/x86-64/acpi.c
+++ boot/kernel/x86-64/acpi.c
@@ -57,39 +57,7 @@ typedef struct {
 
 u64 rsdp_address = 0;
 
-#define ACPI_RSDP_V1_LENGTH 20
-#define ACPI_RSDP_MAX_LENGTH 4096U
-#define ACPI_MAX_TABLE_LENGTH (1024U * 1024U)
 
-static int checksum_ok(const void *address, u32 length) {
-    const u8 *bytes = (const u8 *)address;
-    u8 sum = 0;
-
-    for (u32 i = 0; i < length; i++)
-        sum = (u8)(sum + bytes[i]);
-
-    return sum == 0;
-}
-
-static int sdt_ok(const ACPISDTHeader *table) {
-    if (table == 0 || table->length < sizeof(ACPISDTHeader) ||
-        table->length > ACPI_MAX_TABLE_LENGTH)
-        return 0;
-
-    return checksum_ok(table, table->length);
-}
-
-static u32 read_u32(const u8 *address) {
-    return (u32)address[0] |
-           ((u32)address[1] << 8) |
-           ((u32)address[2] << 16) |
-           ((u32)address[3] << 24);
-}
-
-static u64 read_u64(const u8 *address) {
-    return (u64)read_u32(address) |
-           ((u64)read_u32(address + 4) << 32);
-}
 
 void rsdp_init(u64 rsdp_base) {
     rsdp_address = rsdp_base;
@@ -97,95 +65,46 @@ void rsdp_init(u64 rsdp_base) {
 
 
 u64 discover_APICIO(void) {
-    if (rsdp_address == 0)
-        return 0;
+    RSDPDescriptor20 *rsdp = (RSDPDescriptor20*)rsdp_address;
 
-    RSDPDescriptor20 *rsdp = (RSDPDescriptor20 *)rsdp_address;
+    u64 xsdt_address = rsdp->xsdt_address;
 
-    if (memcmp(rsdp->signature, "RSD PTR ", 8) != 0 ||
-        !checksum_ok(rsdp, ACPI_RSDP_V1_LENGTH))
-        return 0;
+    ACPISDTHeader *xsdt = (ACPISDTHeader*)xsdt_address;
 
-    ACPISDTHeader *root;
-    u32 entry_size;
+    u32 entries =
+        (xsdt->length - sizeof(ACPISDTHeader)) / 8;
 
-    if (rsdp->revision >= 2) {
-        if (rsdp->length < sizeof(RSDPDescriptor20) ||
-            rsdp->length > ACPI_RSDP_MAX_LENGTH ||
-            !checksum_ok(rsdp, rsdp->length) || rsdp->xsdt_address == 0)
-            return 0;
-
-        root = (ACPISDTHeader *)rsdp->xsdt_address;
-        entry_size = 8;
-        if (!sdt_ok(root) || memcmp(root->signature, "XSDT", 4) != 0)
-            return 0;
-    } else {
-        if (rsdp->rsdt_address == 0)
-            return 0;
-
-        root = (ACPISDTHeader *)(u64)rsdp->rsdt_address;
-        entry_size = 4;
-        if (!sdt_ok(root) || memcmp(root->signature, "RSDT", 4) != 0)
-            return 0;
-    }
-
-    u32 payload_length = root->length - sizeof(ACPISDTHeader);
-    if ((payload_length % entry_size) != 0)
-        return 0;
-
-    u32 entries = payload_length / entry_size;
-    u8 *table_addresses = (u8 *)root + sizeof(ACPISDTHeader);
+    u64 *table_addresses =
+        (u64*)((u8*)xsdt + sizeof(ACPISDTHeader));
 
     for (u32 i = 0; i < entries; i++) {
-        const u8 *entry_address = table_addresses + ((u64)i * entry_size);
-        u64 table_address;
-        if (entry_size == 8)
-            table_address = read_u64(entry_address);
-        else
-            table_address = read_u32(entry_address);
+        ACPISDTHeader *table =
+            (ACPISDTHeader*)table_addresses[i];
 
-        if (table_address == 0)
-            continue;
-
-        ACPISDTHeader *table = (ACPISDTHeader *)table_address;
-
-        /* Do not checksum every XSDT/RSDT child.  We only need the MADT, and
-           unrelated firmware tables are not necessarily mapped for use yet. */
         if (memcmp(table->signature, "APIC", 4) == 0) {
-            if (!sdt_ok(table))
-                return 0;
+            MADT *madt = (MADT*)table;
 
-            MADT *madt = (MADT *)table;
+            uint8_t *ptr = madt->entries;
 
-            if (madt->header.length < sizeof(MADT))
-                return 0;
+        while (ptr < ((u8*)madt + madt->header.length))
+        {
+            MADTEntryHeader *entry = (MADTEntryHeader*)ptr;
 
-            u8 *ptr = madt->entries;
-            u8 *end = (u8 *)madt + madt->header.length;
+            if (entry->type == 1)
+            {
+                MADTIOAPIC *ioapic = (MADTIOAPIC*)entry;
 
-            while (ptr + sizeof(MADTEntryHeader) <= end) {
-                MADTEntryHeader *entry = (MADTEntryHeader *)ptr;
-
-                if (entry->length < sizeof(MADTEntryHeader) ||
-                    entry->length > (u64)(end - ptr))
-                    return 0;
-
-                if (entry->type == 1) {
-                    if (entry->length < sizeof(MADTIOAPIC))
-                        return 0;
-
-                    MADTIOAPIC *ioapic = (MADTIOAPIC *)entry;
-
-                    return (u64)ioapic->address;
-                }
-
-                ptr += entry->length;
+                return (u64)ioapic->address;
             }
 
-            return 0;
+            ptr += entry->length;
+        }
+
+        return 1;
+
         }
     }
 
-    return 0;
+    return 1;
 
 }
