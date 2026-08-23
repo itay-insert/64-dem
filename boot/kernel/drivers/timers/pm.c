982d1258fbffb4f5287c6359745bb6198c7a01cf
itay <itay@itay-Inspiron-5559.lan>
2026-08-24 01:44:43 +0300

    continued PM_timer development

--- boot/kernel/drivers/timers/pm.c
+++ boot/kernel/drivers/timers/pm.c
@@ -19,26 +19,92 @@ typedef struct {
     u32 (*read)(u64, u8);
 } __attribute__((packed)) pmctl;
 
+
 void write_mmio(u64 offset, u8 type, u32 value) {
-    switch (type)
-    {
+    switch (type) {
+
         case BYTE:
             u8 val = (u8)value;
-            volatile u8 *ptr = (u8 *)(PM_base + offset);
+            volatile u8 *ptr = (volatile u8 *)(PM_base + offset);
             break;
 
         case WORD:
             u16 val = (u16)value;
-            volatile u16 *ptr = (u16 *)(PM_base + offset);
+            volatile u16 *ptr = (volatile u16 *)(PM_base + offset);
             break;
 
         default:
             u32 val = value;
-            volatile u32 *ptr = (u32 *)(PM_base + offset);
+            volatile u32 *ptr = (volatile u32 *)(PM_base + offset);
             break;
+
     }
 
     *ptr = val;     
+}
+
+
+void write_io(u64 offset, u8 type, u32 value) {
+    u16 port = ((u16)PM_base + (u16)offset);
+
+    switch (type) {
+        case BYTE:
+            outb(port, (u8)value);
+            break;
+
+        case WORD:
+            outw(port, (u16)value);
+            break;
+
+        default:
+            outl(port, value);
+            break;
+
+    }
+
+}
+
+
+u32 read_mmio(u64 offset, u8 type) {
+    switch (type) {
+
+        case BYTE:
+            volatile u8 *ptr = (volatile u8 *)(PM_base+offset);
+            break;
+
+        case WORD:
+            volatile u16 *ptr = (volatile u16 *)(PM_base+offset);
+            break;
+
+        default:
+            volatile u32 *ptr = (volatile u32 *)(PM_base+offset);
+            break;
+    }
+
+    return ((u32)*ptr);
+}
+
+
+u32 read_io(u64 offset, u8 type) {
+    u16 port = ((u16)PM_base + (u16)offset);
+
+    switch (type) {
+
+        case BYTE:
+            u8 val = inb(port);
+            break;
+
+        case WORD:
+            u16 val = inw(port);
+            break;
+
+        default:
+            u32 val = inl(port);
+            break;
+
+    }
+
+    return ((u32)val);
 }
 
 
