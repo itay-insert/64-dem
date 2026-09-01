6c2f1f9d2f2f018089282f4f9b1f74c3427e0b78
itay <itay@itay-Inspiron-5559.lan>
2026-09-01 11:35:04 +0300

    va_alloc

--- boot/kernel/x86-64/memory/va_alloc.c
+++ boot/kernel/x86-64/memory/va_alloc.c
@@ -16,7 +16,14 @@ extern u64 Kernel_end;
 
 #define SPACE_END 0xFFFFFFFFFFFFFFFF
 
-static inline va_node *find_new_entry(void) {
+#define USR (attributes & 0x04)
+
+typedef enum {
+    left = 0,
+    right = 1,
+};
+
+static inline va_node *find_new_entry(u8 Pos, va_node *Parent) {
     va_node *entry = NULL;
     va_hd *hd = va_header;
     while (hd->free_entries == NoEntriesLeft) {
@@ -38,6 +45,13 @@ static inline va_node *find_new_entry(void) {
     if (entry == NULL) {
         entry = (va_node *)((u8 *)va_latest + sizeof(va_node));
     }
+
+    Parent->va_right = Pos == right ? entry : NULL;
+    Parent->va_left = Pos == left ? entry : NULL;
+
+    va_latest = entry;
+    entries++;
+    hd->free_entries--;
 
     return entry;
 }
@@ -65,6 +79,68 @@ va_ret va_alloc(u64 pages, u16 attributes) {
         va_start->virtual_base = (Kernel_end + 4095) & ~0xFFF;
         va_start->length = (END - va_start->virtual_base) + 1;
         va_latest = va_start;
-        
+        va_node *right_node = find_new_entry(right, va_start);
+        right_node->virtual_base = KERNEL_SPACE;
+        right_node->attributes = 0x03;
+        right_node->length = (SPACE_END - KERNEL_SPACE) + 1;
+        va_node *left_node = find_new_entry(left, va_start);
+        left_node->virtual_base = USER_SPACE;
+        left_node->attributes = 0x07;
+        left_node->length = (USER_END - USER_SPACE) + 1;
+        left_node->max_length = left_node->length;
+        right_node->max_length = right_node->length;
+        va_start->max_length = left_node->max_length;
     }
+
+
+    va_node *entry = NULL;
+
+    u64 rsz = pages << 12;
+    if (!USR) {
+        entry = va_start->va_right;
+    } else {
+        entry = va_start->va_left;
+    }
+
+    while (1) {
+
+        if (entry->length >= rsz) {
+            EFI_MEMORY_DESCRIPTOR alloc = vmalloc(entry->virtual_base, pages, attributes);
+            if (alloc.Attribute != 0) {
+                spin_unlock(&va_lock);
+                ret.status = 1;
+                return ret;
+            }
+    
+            if (entry->length == entry->max_length) 
+                entry->max_length -= rsz;
+        
+            entry->length -= rsz;
+
+            entry->virtual_base += rsz;
+
+            spin_unlock(&va_lock);
+
+            ret.base = alloc.VirtualStart;
+            ret.pages = pages;
+            ret.attributes = attributes;
+            ret.status = 0;
+            return ret;
+        } else if (entry->max_length >= rsz) {
+            va_node *left_side = entry->va_left;
+            va_node *right_side = entry->va_right;
+
+            if (left_side->length >= rsz) {
+                entry = left_side;
+            } else if (right_side->max_length >= rsz) {
+                entry = right_side;
+            } else {
+                spin_unlock(&va_lock);
+                ret.status = 1;
+                return ret;
+            }
+        }
+
+    }
+    
 }
